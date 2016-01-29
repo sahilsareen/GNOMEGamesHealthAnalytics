eb59f0e5ad794acb4fb3682d41caca0e7f1d2e5e
Sahil Sareen <sahil.sareen@hotmail.com>
2016-01-29 22:37:12 +0530

    Add config and config reader
      - Specify:
        - which modules are of interest
        - where to pull them from
        - which build to check health of modules

---
+++ src/GamesConfigExtractor.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2016 Sahil Sareen (ssareen [AT] gnome [DOT] org)
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 2 of the License, or (at your option) any later
+# version. See http://www.gnu.org/copyleft/gpl.html the full text of the
+# license.
+
+# Read the games configuration from the config file
+# present in resources(games.json)
+# TODO: Upgrade to typesafe config: https://github.com/chimpler/pyhocon
+
+
+import json
+
+
+class GamesConfigExtractor:
+    def __init__(self, file_name="games.json"):
+        with open(file_name) as data_file:
+            config = json.load(data_file)
+
+        self.games = config["games"]
+        self.github_raw_url = config["github_raw_configure"]
+        self.build_url = config["build_url"] % config["date"]
