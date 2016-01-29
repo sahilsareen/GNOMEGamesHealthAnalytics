2077f20612ae34af678c46a79aed284846949b34
Sahil Sareen <sahil.sareen@hotmail.com>
2016-01-29 22:47:01 +0530

    Add a BuildStatusChecker
      - Parse json content pulled from a url
      - Check if the module built succesfully

---
+++ src/BuildStatusChecker.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2016 Sahil Sareen (ssareen [AT] gnome [DOT] org)
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 2 of the License, or (at your option) any later
+# version. See http://www.gnu.org/copyleft/gpl.html the full text of the
+# license.
+
+import urllib, json
+
+
+class BuildStatusChecker:
+    def __init__(self, url):
+        response = urllib.urlopen(url)
+        self.data = json.loads(response.read())
+        self.build_status = {}
+
+    def get_game_status(self, game):
+        for item in self.data["apps"]:
+            if item["id"].find(game) is not -1:
+                return item["state"] == "success"
+
+    def get_status(self, games):
+        for game in games:
+            self.build_status[game] = self.get_game_status(game)
+        return self.build_status
